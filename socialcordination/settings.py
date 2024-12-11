677dc6ad7f17f89af7929657b635cde994047287
Nandana19052003 <b.nandana12d@gmail.com>
2024-12-11 15:21:48 +0530

    postgre and xtraimage delete

--- socialcordination/settings.py
+++ socialcordination/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'socialcordination.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nextgenevents',
+        'USER': 'postgres',
+        'PASSWORD': 'newpassword',
+        'HOST': 'localhost',  
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
